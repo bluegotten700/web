3847b00f191a7b16c92412e900b0348b033e411b
tra chan <220441213+bluegotten700@users.noreply.github.com>
2025-12-13 11:02:21 +0700

    Update app title to 'Tra Chan Nonprofit School'

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Tra Chan Nonprofit School',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
